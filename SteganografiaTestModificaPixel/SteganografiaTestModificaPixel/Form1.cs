ab88ea778a3010ed8f1a6d43885db209ea99ebeb
FanteyOfficial <81305405+FanteyOfficial@users.noreply.github.com>
2023-01-10 01:00:24 +0100

    Add files via upload

--- SteganografiaTestModificaPixel/SteganografiaTestModificaPixel/Form1.cs
+++ SteganografiaTestModificaPixel/SteganografiaTestModificaPixel/Form1.cs
@@ -11,15 +11,11 @@ namespace SteganografiaTestModificaPixel
     public partial class Form1 : Form
     {
         Bitmap bitmap;
+        String ext = "";
 
         public Form1()
         {
             InitializeComponent();
-        }
-
-        public void loadImage()
-        {
-            
         }
 
         private void imageSelectionBTN_Clicked(object sender, EventArgs e)
@@ -27,7 +23,7 @@ namespace SteganografiaTestModificaPixel
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Title = "Select Image";
             openFileDialog.InitialDirectory = @"C:\Downloads";
-            openFileDialog.Filter = "PNG (*.PNG)|*.PNG|JPG (*.JPG)|*.JPG";
+            openFileDialog.Filter = "PNG (*.PNG)|*.PNG|JPG (*.JPG)|*.JPG|BMP (*.BMP)|*.BMP|JPEG (*.JPEG)|*.JPEG";
             openFileDialog.Multiselect = false;
             openFileDialog.FilterIndex = 2;
             openFileDialog.ShowDialog();
@@ -35,106 +31,159 @@ namespace SteganografiaTestModificaPixel
             {
                 CurrentImage.ImageLocation = openFileDialog.FileName;
                 bitmap = new Bitmap(openFileDialog.FileName);
+                if (openFileDialog.FileName.EndsWith(".png") || openFileDialog.FileName.EndsWith(".PNG")) {
+                    bitmap = bitmap.Clone(new Rectangle(0, 0, bitmap.Width, bitmap.Height), System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                }
+                ext = Path.GetExtension(openFileDialog.FileName);
             }
         }
 
         private void hideTextBTN_Clicked(object sender, EventArgs e)
         {
             String text = TextToHide.Text;
+            Color pixel;
+            int x = 0, y = 0;
 
-            // Convert the message to a byte array
-            byte[] messageBytes = System.Text.Encoding.UTF8.GetBytes(text);
+            // convert all string message in binary
+            byte[] bytes = Encoding.ASCII.GetBytes(text);
+            //MessageBox.Show("" + bytes.Length);
+            //for (int a=0; a<bytes.Length; a++) MessageBox.Show("" + bytes[a]);
 
-            // Loop through the pixels in the image
-            for (int i = 0; i < bitmap.Height; i++)
+            string textInBinary = "";
+            foreach (byte b in bytes)
             {
-                for (int j = 0; j < bitmap.Width; j++)
+                textInBinary += Convert.ToString(b, 2).PadLeft(8, '0');
+            }
+            //MessageBox.Show("" + textInBinary.Length);
+
+            if (bitmap.Width * bitmap.Height < textInBinary.Length)
+            {
+                MessageBox.Show("Impossibile nascondere il messaggio nell'immagine!");
+            }
+            else
+            {
+                for (int i=0; i<textInBinary.Length; i++)
                 {
-                    // Get the pixel at the current position
-                    Color pixel = bitmap.GetPixel(i, j);
+                    // verify coordinates in bitmap
+                    if (x < bitmap.Width-1)
+                    {
+                        x++;
+                    }
+                    else
+                    {
+                        x = 0;
+                        y++;
+                    }
 
-                    // Change the least significant bit of the pixel's red value
-                    // to the next bit in the message
-                    int newRedValue = pixel.R;
-                    newRedValue = (newRedValue & ~(1 << 7)) | ((messageBytes[i] & 1) << 7); // MSB
-                    //newRedValue = (newRedValue & ~1) | (messageBytes[i] & 1); // LSB
-                    Color newPixel = Color.FromArgb(newRedValue, pixel.G, pixel.B);
-                    bitmap.SetPixel(j, i, newPixel);
+                    // modify pixel
+                    pixel = bitmap.GetPixel(x, y);
+                    byte pixelValue;
+                    pixelValue = pixel.R;
+                    String pixelInBinary = "";
+                    pixelInBinary = Convert.ToString(pixelValue, 2).PadLeft(8, '0');
+                    String newColorBinary = "";
 
-                    // Shift the message bytes to the right by 1 bit
-                    //messageBytes = messageBytes.Select(x => x >> 1).ToArray();
+                    if (LSB_Mode.Checked)
+                    {
+                        newColorBinary = pixelInBinary.Substring(0, 6);
+                        newColorBinary += textInBinary[i];
+                    }
+                    else if (MSB_Mode.Checked)
+                    {
+                        newColorBinary = pixelInBinary.Substring(1, 7);
+                        newColorBinary = textInBinary[i] + newColorBinary;
+                        //MessageBox.Show(newColorBinary, "Nuovo");
+                        //MessageBox.Show(pixelInBinary, "Vecchio");
+                    }
+
+                    byte newColor;
+                    newColor = Convert.ToByte(newColorBinary, 2);
+
+                    bitmap.SetPixel(x, y, Color.FromArgb(newColor, pixel.G, pixel.B));
+                }
+
+                String finalChr = "00011000";
+                // add final character
+                for (int i=0; i<finalChr.Length; i++)
+                {
+                    if (x < bitmap.Width - 1)
+                    {
+                        x++;
+                    }
+                    else
+                    {
+                        x = 0;
+                        y++;
+                    }
+
+                    pixel = bitmap.GetPixel(x, y);
+                    byte pixelValue;
+                    pixelValue = pixel.R;
+                    String pixelInBinary = "";
+                    pixelInBinary = Convert.ToString(pixelValue, 2).PadLeft(8, '0');
+                    String newColorBinary = "";
+
+                    if (LSB_Mode.Checked)
+                    {
+                        newColorBinary = pixelInBinary.Substring(0, 6);
+                        newColorBinary += finalChr[i];
+                    }
+                    else if (MSB_Mode.Checked)
+                    {
+                        newColorBinary = pixelInBinary.Substring(1, 7);
+                        MessageBox.Show(newColorBinary);
+                        newColorBinary = finalChr[i] + newColorBinary;
+                    }
+
+                    byte newColor;
+                    newColor = Convert.ToByte(newColorBinary, 2);
+
+                    bitmap.SetPixel(x, y, Color.FromArgb(newColor, pixel.G, pixel.B));
+                }
+
+                CurrentImage.Image = bitmap;
+                bitmap.Save("abc" + ext);
+                CurrentImage.ImageLocation = "./abc" + ext;
+                MessageBox.Show("Fatto!");
+            }
+        }
+
+        private void DecodeTXT_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Inizio");
+            String txt = "";
+            Color pixel;
+            String px = "";
+            String specialChr = "00011000";
+            for (int i=0; i<bitmap.Height; i++)
+            {
+                for (int j = 0; j<bitmap.Width; j++)
+                {
+                    pixel = bitmap.GetPixel(j, i);
+
+                    if (LSB_Mode.Checked) px += Convert.ToString(pixel.R, 2).PadLeft(8, '0')[7];
+                    else if (MSB_Mode.Checked) px += Convert.ToString(pixel.R, 2).PadLeft(8, '0')[0];
+
+                    if (px.Equals(specialChr))
+                    {
+                        MessageBox.Show("Fine");
+                        TextToHide.Text = txt;
+                        return;
+                    }
+
+                    if (px.Length == 8)
+                    {
+                        // MessageBox.Show("" + px.Length);
+                        int n;
+                        n = Convert.ToInt32(px, 2);
+                        txt += Convert.ToChar(n);
+                        px = "";
+                    }
                 }
             }
-
-            // update open
-            /*byte[] txtBytes = System.Text.Encoding.UTF8.GetBytes(text); // create array with ascii code of every letter in String
-            //BitArray bitArray = new BitArray(txtBytes);
-            String binaryText = "";
-            for (int i=0; i<txtBytes.Length; i++)
-            {
-                binaryText += Convert.ToString(txtBytes[i], 2).PadLeft(8, '0');
-            }
-
-            Color px;
-            int x = 0;
-            int y = 0;
-
-            for (int i=0; i < binaryText.Length; i++)
-            {
-                //for (int j=0; j<8; j++)
-                //{
-                if (x < bitmap.Width-1)
-                {
-                    x++;
-                }
-                else
-                {
-                    y++;
-                    x = 0;
-                }
-                px = bitmap.GetPixel(x, y);*/
-            // updated close
-
-            //String red = Convert.ToString(px.R, 2).PadLeft(8, '0');
-            //byte temp = px.R;
-
-            /*byte[] newPxR = System.Text.Encoding.UTF8.GetBytes(Convert.ToString(px.R, 2).PadLeft(8, '0'));
-            BitArray r = new BitArray(newPxR);
-
-            if (r[0] != bitArray[i])
-            {
-                r[0] = bitArray[i];
-            }*/
-
-            // update open
-            /*String binary = "";
-            for (int k = 0; k < System.Text.Encoding.UTF8.GetBytes(Convert.ToString(px.R)).Length; k++)
-            {
-                binary += Convert.ToString(System.Text.Encoding.UTF8.GetBytes(Convert.ToString(px.R))[k], 2).PadLeft(8, '0');
-            }
-            String res = binary;
-            //String[] resSplit = res.Split();
-            String[] resSplit = res.Select(c => c.ToString()).ToArray();
-            if (!resSplit[7].Equals(Convert.ToString(binaryText[i])))
-            {
-                resSplit[7] = Convert.ToString(binaryText[i]);
-            }
-            res = String.Join("", resSplit);
-
-            byte[] red = Encoding.ASCII.GetBytes(res);
-            //MessageBox.Show(Convert.ToString(red[0]));
-            bitmap.SetPixel(x, y, Color.FromArgb(red[0], px.G, px.B, px.A));
-            //}
-        }*/
-            //update closed
-            /*for (int i = 0; i < txtBytes.Length; i++)
-            {
-                
-                px = bitmap.GetPixel(i / bitmap.Width, i / bitmap.Height);
-                bitmap.SetPixel(i / bitmap.Width, i / bitmap.Height, Color.FromArgb(px.R, px.G, px.B, px.A & 0xFC | txtBytes[i]));
-            }*/
-            bitmap.Save("abcd.png");
-            MessageBox.Show("Fatto!");
+            MessageBox.Show("Fine2");
+            TextToHide.Text = txt;
+            return;
         }
     }
 }
